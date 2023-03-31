928cbd4f164d6c43a97d1c26ffca0694882c08f6
aliyashine <127969225+aliyashine@users.noreply.github.com>
2023-03-31 11:20:34 -0400

    JavaScript Page

---
+++ main.js
@@ -0,0 +1,21 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    console.log("scroll");
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
+const checkbtn = document.querySelector(".checkbtn");
+
+window.addEventListener("scroll", () => {
+    console.log("scroll");
+    if (window.pageYOffset > 80) {
+        checkbtn.classList.add("active");
+    } else {
+        checkbtn.classList.remove("active");
+    }
+})
